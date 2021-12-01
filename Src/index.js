86f93b4c6fe6098b565076296639ae722df94e49
Dhanmoni Nath <dhanmoni@users.noreply.github.com>
2021-12-01 17:32:03 +0530

    firestore linked to app

--- Src/index.js
+++ Src/index.js
@@ -1,1 +1,34 @@
-console.log("Welcome to firebase v9 from index.js")
+// console.log("Welcome to firebase v9 from index.js")
+import {initializeApp} from 'firebase/app'
+
+import {getFirestore, collection, getDocs} from 'firebase/firestore'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC8CEIALd7kjuUaUEhVOLI5oBxHTUGzBfw",
+  authDomain: "ninja-cloud-functions-f1052.firebaseapp.com",
+  projectId: "ninja-cloud-functions-f1052",
+  storageBucket: "ninja-cloud-functions-f1052.appspot.com",
+  messagingSenderId: "307744957213",
+  appId: "1:307744957213:web:92a31a7d3fd526e57eb7f4"
+};
+
+//initialize app
+initializeApp(firebaseConfig)
+
+//init service
+const db = getFirestore()
+
+//collection ref
+const colRef = collection(db, 'books')
+
+//get firestore data
+getDocs(colRef)
+  .then(snapshot=> {
+    let books = [];
+    snapshot.docs.forEach(doc=> {
+      books.push({...doc.data(), id: doc.id})
+    })
+    console.log(books)
+  })
+  .catch(err=>console.log(err))
